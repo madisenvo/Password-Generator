3326ef8c56625ce7cd7d7e57980652698f3644f2
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-08-25 15:56:35 -0600

    added button

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var goBtn = document.querySelector("go");
 
 // my additions
 var numberChars = "0123456789";
@@ -16,8 +17,13 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  function writePassword(){
+generateBtn.addEventListener("click", openOptions);
+  function openOptions(){
     var options = document.getElementById("popup");
     options.classList.toggle("show");
+  }
+
+goBtn.addEventListener("click", writePassword);
+  function writePassword(){
+   
   }
