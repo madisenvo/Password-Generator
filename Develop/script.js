007ea2a50d473ed09bb3973cd6ecfe8321e27a9c
Madisen <madisenvogel@gmail.com>
2022-08-28 17:54:35 -0600

    prompts human-proofed

--- Develop/script.js
+++ Develop/script.js
@@ -5,18 +5,23 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberChar = "1234567890";
 
+// User prompts
 function generatePassword() {
   var pLength = prompt("Enter your desired number of characters (Note: must be between 12 and 128).");
     if (pLength < 12 || pLength > 128){
-      alert("Length must be 8-128 characters");
+      alert("Length must be between 12 and 128 characters.");
     } else{
       var addUpper = confirm("Include uppercase letters?")
       var addLower = confirm("Include lowercase letters?")
       var addNum = confirm("Include numbers?")
       var addSpecial = confirm("Include special characters?")
-      }  
-}
+      } 
+    if (addUpper != true && addLower != true && addNum != true && addSpecial != true){
+      alert("You must select at least one character type.")
+    }
 
+    
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -25,7 +30,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-var generateButton = document.getElementById("generate");
-generateButton.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
 
